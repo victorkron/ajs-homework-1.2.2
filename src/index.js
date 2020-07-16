574b707aab0a2fb11eebde6dbfc3a54651a16a12
Karim <k.kron2015@yandex.ru>
2020-07-16 16:56:25 +0300

    delete test.html

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/style.css';
 
-import './js/app';
+import './js/App/app';
 
 // TODO: write your code in app.js
--- src/js/App/app.js
+++ src/js/App/app.js
@@ -1,5 +1,3 @@
 import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from '../Game/game.js';
 const game = new Game();
 game.start();
-
-// Если открыть файл test.html в консоли увидим 'game started'
